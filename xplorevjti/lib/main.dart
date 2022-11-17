a73a936e6bd461c72b4368666aa27f1fc662138a
Arsh Khan <khanarsh0124@gmail.com>
2022-11-17 20:10:26 +0530

    Added Main UI with signout option

--- xplorevjti/lib/main.dart
+++ xplorevjti/lib/main.dart
@@ -3,7 +3,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:xplorevjti/views/login_view.dart';
 import 'package:xplorevjti/views/register_view.dart';
-// import 'package:xplorevjti/views/register_view.dart';
+import 'package:xplorevjti/views/verify_email_view.dart';
+import 'dart:developer' as devtools show log;
 
 import 'firebase_options.dart';
 
@@ -37,20 +38,90 @@ class HomePage extends StatelessWidget {
           switch (snapshot.connectionState) {
             case ConnectionState.done:
               // TODO: Handle this case.
-              // Anonymous Route
-              // final user = FirebaseAuth.instance.currentUser;
-              // if (user?.emailVerified ?? false) {
-              //   return const Text('Done');
-              // } else {
-              //   // Navigator.of(context).push(MaterialPageRoute(
-              //   //   builder: (context) => const VerifyEmailView(),
-              //   // ));
-              //   return const VerifyEmailView();
-              // }
-              return const LoginView();
+              final user = FirebaseAuth.instance.currentUser;
+              if (user != null) {
+                if (user.emailVerified) {
+                  print('User is Verified');
+                  return const NotesView();
+                } else {
+                  // Anonymous Route
+                  // Navigator.of(context).push(MaterialPageRoute(
+                  //   builder: (context) => const VerifyEmailView(),
+                  // ));
+                  return const VerifyEmailView();
+                }
+              } else {
+                return const LoginView();
+              }
             default:
               return const CircularProgressIndicator();
           }
         });
   }
+}
+
+enum MenuAction { logout }
+
+class NotesView extends StatefulWidget {
+  const NotesView({super.key});
+
+  @override
+  State<NotesView> createState() => _NotesViewState();
+}
+
+class _NotesViewState extends State<NotesView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text('Main UI'), actions: [
+          PopupMenuButton<MenuAction>(onSelected: (value) async {
+            // devtools.log(value.toString());
+            switch (value) {
+              case MenuAction.logout:
+                // TODO: Handle this case.
+                final shouldLogout = await showLogOutDialog(context);
+                // devtools.log(shouldLogout.toString());
+                if (shouldLogout) {
+                  await FirebaseAuth.instance.signOut();
+                  Navigator.of(context).pushNamedAndRemoveUntil(
+                    '/login/',
+                    (_) => false,
+                  );
+                }
+                break;
+            }
+          }, itemBuilder: (context) {
+            return [
+              const PopupMenuItem<MenuAction>(
+                  value: MenuAction.logout, child: Text('Log Out')),
+            ];
+          })
+        ]),
+        body: const Text('Hello World'));
+  }
+}
+
+//bool is perfect either logout or cancel
+Future<bool> showLogOutDialog(BuildContext context) {
+  return showDialog<bool>(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Sign Out'),
+          content: const Text('Are you sure you want to sign out'),
+          actions: [
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(false);
+                },
+                child: const Text('Cancel')),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+              child: const Text('Logout'),
+            )
+          ],
+        );
+      }).then((value) => value ?? false);
 }
