58c1e763525ce7fb8ee571c23bbfbbe3816dc66b
Arsh Khan <khanarsh0124@gmail.com>
2022-11-22 23:42:43 +0530

    Added unit testing code for our auth service

--- xplorevjti/lib/services/auth/auth_user.dart
+++ xplorevjti/lib/services/auth/auth_user.dart
@@ -5,7 +5,8 @@ import 'package:flutter/foundation.dart';
 class AuthUser {
   final bool isEmailVerified;
 
-  const AuthUser(this.isEmailVerified);
+  const AuthUser({required this.isEmailVerified});
 
-  factory AuthUser.fromFirebase(User user) => AuthUser(user.emailVerified);
+  factory AuthUser.fromFirebase(User user) =>
+      AuthUser(isEmailVerified: user.emailVerified);
 }
