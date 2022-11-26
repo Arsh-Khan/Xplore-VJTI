5333ceaab4bc8d41656b3cfff736cd19809fd2c4
Ruturaj Rao <rutu22.rao@gmail.com>
2022-11-27 00:10:34 +0530

    modified main.dart

--- xpl_vjti/lib/main.dart
+++ xpl_vjti/lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:xplorevjti/pages/Sign_Up.dart';
-import 'package:xplorevjti/pages/homescreen.dart';
 import 'pages/login_vjti.dart';
 
 void main() {
   runApp(MaterialApp(
 
-    home: homescreen(), 
+    home: login_vjti(), 
 
   ));
 }
